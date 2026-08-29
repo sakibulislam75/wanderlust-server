0d28a67b11da788d8256847b7e10ee135fce792f
sakibulislam75 <sakibulislam1623@gmail.com>
2026-08-29 21:32:29 +0600

    update: update cors using method

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+   cors({
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+   }),
+);
 app.use(express.json());
-
 
 const port = process.env.PORT;
 const client = new MongoClient(process.env.MONGODB_URI);
